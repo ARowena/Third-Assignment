ffe91d4dec7de88ad6f6d97c8e5a0d6856486e3b
gtarriba <gabriel.tarriba@gmail.com>
2015-11-12 10:34:59 +0100

    Deleted unnecessary code

--- conflict_data.R
+++ conflict_data.R
@@ -7,17 +7,8 @@
 #########################################################################################
 
 # 1. Download the data from website of Uppsala University's Department of Peace and Conflict Research
-UrlAddress <- "http://www.pcr.uu.se/digitalAssets/124/124920_1ucdp-prio-2015.rdata"
-DataUrl <- RCurl::getURL(UrlAddress)
-conflict_data <- read.table(textConnection(DataUrl),
-                          sep = ",", header = TRUE)
-
 download.file("http://www.pcr.uu.se/digitalAssets/124/124920_1ucdp-prio-2015.rdata", "temp.rData")
 load("temp.rData")
 
-URL <- "http://www.pcr.uu.se/digitalAssets/124/124920_1ucdp-prio-2015.rdata"
-temp <- tempfile()
-download.file(URL, temp)
-conflict_data <- read
-(temp, 7, sheetName = NULL, startRow = 14, endRow = 230, colIndex = NULL, as.data.frame = TRUE, header = FALSE)
-unlink(temp)
+
+
